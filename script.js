1d29f00af1e4ccda0a777afc0f05c9f2c6a6e496
Nama Anda <email_github_anda@example.com>
2026-07-03 13:37:40 +0700

    fix double fetch error

--- script.js
+++ script.js
@@ -65,3 +65,14 @@ async function fetchTasks() {
 }
 
 fetchTasks();
+
+document.getElementById('btn-simpan').addEventListener('click', async () => {
+    const input = document.getElementById('input-tugas');
+    await fetch('https://tugas-praktikum-api.onrender.com/tasks', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: input.value, status: 'Belum Selesai' })
+    });
+    input.value = '';
+    fetchTasks();
+});
